490c9fea8b12987d04390fd5179374db675a13e8
@Jaival_patni <jaival@tatvic.com>
2024-06-05 18:19:08 +0530

    temp.py file updated again

--- temp.py
+++ temp.py
@@ -46,4 +46,4 @@ def run_query():
         return jsonify({'error': error_message}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
